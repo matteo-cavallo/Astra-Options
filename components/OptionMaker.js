9b0f1d4dc25b868f8c6c142606bef81b17d722a3
Andrei Calin <calinvasileandrei@gmail.com>
2020-11-28 13:26:51 +0100

    Pre Redux update

--- components/OptionMaker.js
+++ components/OptionMaker.js
@@ -1,26 +1,47 @@
 import React,{useState,useCallback} from "react";
-import { Card, Layout, TextField,FormLayout,Button } from "@shopify/polaris";
+import { Card, TextField,FormLayout,Button,Select } from "@shopify/polaris";
 
-function OptionMaker ({name,remove}) {
-  const [optionForm,setOptionForm] = useState({});
-  const handleOptionFormChange = useCallback((value,field) =>{ 
-    setOptionForm({...optionForm,[field]:value}),[];
+
+
+function OptionMaker ({name,remove,saveForm}) {
+  const inputTypeOptions =[
+    {label: 'TextField', value: 'textfield'},
+    {label: 'Select', value: 'select'},
+    {label: 'Radius', value: 'radius'},
+    {label: 'CheckBox', value: 'checkbox'},
+  ];
+
+  const [optionForm,setOptionForm] = useState({type:"textfield"});
+
+  const handleOptionFormChange  =  useCallback((value,field) =>{ 
+    setOptionForm({...optionForm,[field]:value});
     console.log(optionForm)
-});
+  });  
+
+
   return (
       <Card title={name} sectioned>
         <FormLayout>
           <FormLayout.Group condensed>
-            <TextField label="Title"  value={optionForm.title} onChange={(value) => {handleOptionFormChange(value,"title")}} />
+            <TextField label="Title"  value={optionForm.title} onChange={(value) => {handleOptionFormChange(value,"title");}} />
             <TextField label="Description" value={optionForm.description} onChange={(value) => {handleOptionFormChange(value,"description")}} />
-            <TextField label="Input Type" value={optionForm.type} onChange={(value) => {handleOptionFormChange(value,"type")}} />
+            <Select    label="Input Type" options={inputTypeOptions} value={optionForm.type} onChange={(value) => {handleOptionFormChange(value,"type")}} />
             <TextField label="Required" value={optionForm.required} onChange={(value) => {handleOptionFormChange(value,"required")}} />
             <TextField label="Price" value={optionForm.price} onChange={(value) => {handleOptionFormChange(value,"price")}} />
             <TextField label="MoreOptions" value={optionForm.moreoption1} onChange={(value) => {handleOptionFormChange(value,"moreoption1")}} />
             <TextField label="MoreOptions2" value={optionForm.moreoption2} onChange={(value) => {handleOptionFormChange(value,"moreoption2")}} />
           </FormLayout.Group>
         </FormLayout><br/>
-        <Button onClick={()=>remove(name)}>Remove</Button>
+        <div style={{display: "flex"}}>
+          <div style={{ color: "#bf0711",margin:"3px" }}>
+            <Button monochrome outline onClick={()=>remove(name)}>Remove</Button>
+          </div>
+
+          <div style={{ color: "#5865C2",margin:"3px" }}>
+            <Button monochrome outline onClick={()=>saveForm(name,optionForm)}>Save</Button>
+          </div>
+        </div>
+        
       </Card>
   );
 }
--- pages/template.js
+++ pages/template.js
@@ -1,51 +1,129 @@
-import { Heading, Page, Layout, EmptyState, Card,TextField,Button } from "@shopify/polaris";
-import React,{ useState,useCallback,useReducer } from "react";
-import { act } from "react-test-renderer";
-import OptionMaker from '../components/OptionMaker.js';
+import {
+  Page,
+  Layout,
+  Card,
+  TextField,
+  Button,
+  InlineError,
+} from "@shopify/polaris";
+import React, { useState, useCallback, useReducer } from "react";
+import OptionMaker from "../components/OptionMaker.js";
 
-
-const optionsReducer=(state,action)=>{
-  switch(action.type){
-    case 'add':
-      return [...state,action.option];
-    case 'remove':
-      const productIndex = state.findIndex(item => item.key == action.key);
-      if(productIndex < 0) {
+const optionsReducer = (state, action) => {
+  switch (action.type) {
+    case "add":
+      return [...state, action.form];
+    case "remove": {
+      let productIndex = state.findIndex((item) => item.key == action.key);
+      if (productIndex < 0) {
         return state;
       }
-      const update = [...state];
-      update.splice(productIndex, 1)
-      return update
+      let update = [...state];
+      update.splice(productIndex, 1);
+      return update;
+    }
+    case "saveForm": {
+      let productIndex = state.findIndex((item) => item.key == action.key);
+      if (productIndex < 0) {
+        return state;
+      }
+      let update = [...state];
+      update[productIndex] = {
+        ...update[productIndex],
+        formData: action.formData,
+      };
+      return update;
+    }
     default:
       return state;
   }
-}
+};
 
 export default function Template({ shopOrigin }) {
-  const [templateNameValue, setTemplateNameValue] = useState('');
-  const [optionNameValue,setOptionNameValue] = useState('');
+  //Text State Manager
+  const [templateNameValue, setTemplateNameValue] = useState("");
+  const [optionNameValue, setOptionNameValue] = useState("");
+  //Validation
+  const [errorMessage, setErrorMessage] = useState({});
+  //List State Management Reducer
+  const [theOptions, setTheOptions] = useReducer(optionsReducer, []);
+  //TextHandlers
+  const handleTemplateNameChange = useCallback(
+    (value) => setTemplateNameValue(value),
+    []
+  );
+  const handleOptionNameChange = useCallback(
+    (value) => setOptionNameValue(value),
+    []
+  );
 
-  const [theOptions,setTheOptions] = useReducer(optionsReducer,[]);
 
-  const handleTemplateNameChange = useCallback((value) => setTemplateNameValue(value),[]);
-  const handleOptionNameChange = useCallback((value)=> setOptionNameValue(value),[]);
+  //validation method
+  const checkName = (thiskey) => {
+    let state = [...theOptions];
+    let productIndex = -1;
+    productIndex = state.findIndex((item) => item.key == thiskey);
+    if (productIndex == -1) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const add = () => {
+    if (optionNameValue != null &&optionNameValue != "") {
+      if(!checkName(optionNameValue)){
+        //remove previus errors
+        if (errorMessage.OptionName != "")setErrorMessage({ ...errorMessage, OptionName: "" ,OptionError:""});
+
+        let option = {
+          key: optionNameValue,
+          form: (
+            <OptionMaker
+              key={optionNameValue}
+              id={optionNameValue}
+              name={optionNameValue}
+              remove={remove}
+              saveForm={saveForm}
+            />
+          ),
+        };
+        setTheOptions({ form: option, type: "add" });
+        setOptionNameValue("");
+
+      }else{
+      setErrorMessage({ ...errorMessage, OptionName: "error", OptionError:"Option name already used!" });
+      }
+    } else {
+      setErrorMessage({ ...errorMessage, OptionName: "error", OptionError:"Option name is required!" });
+    }
+  };
+
+  const remove = (mykey) => {
+    setTheOptions({ key: mykey, type: "remove" });
+  };
+
+  const saveForm = (mykey, formData) => {
+    setTheOptions({ key: mykey, type: "saveForm", formData: formData });
+    console.log(theOptions)
+  };
 
 
-  const add =()=>{
-    let option = {'key':optionNameValue,obj: <OptionMaker 
-      key={optionNameValue} 
-      id={optionNameValue} 
-      name={optionNameValue} 
-      remove={remove}
-    />}
-    setTheOptions({option,type:'add'});
-    setOptionNameValue("");
+  //Final method for saving all the template
+  const saveTemplate = () => {
+    if (templateNameValue != null && templateNameValue != "") {
+      //remove previus errors
+      if (errorMessage.TemplateName != "")setErrorMessage({ ...errorMessage, TemplateName: "" });
 
-  }
+      
 
-  const remove =(key)=>{
-    setTheOptions({'key':key,type:'remove'});
-  }
+    } else {
+      setErrorMessage({ ...errorMessage, TemplateName: "NoTemplateName" });
+    }
+    //TODO:get template name
+    //TODO:get the options
+    //TODO:push to firebase
+  };
 
   return (
     <Page>
@@ -54,30 +132,52 @@ export default function Template({ shopOrigin }) {
           <Layout>
             <Layout.Section>
               <Card title="Template Layout" sectioned>
-                {theOptions.length == 0 ?  <h1>No Options Added</h1> : theOptions.map(option=> option.obj)}
+                {theOptions.length == 0 ? (
+                  <h1>No Options Added</h1>
+                ) : (
+                  theOptions.map((option) => option.form)
+                )}
               </Card>
             </Layout.Section>
             <Layout.Section secondary>
-            <Card title="Options" sectioned>
+              <Card title="Options" sectioned>
                 <TextField
                   label="Template Name"
                   value={templateNameValue}
                   onChange={handleTemplateNameChange}
                   placeholder="Example: ClothesTemplate "
                 />
-                <br/>
-                <hr/>
+                <br />
+                <Button primary onClick={saveTemplate}>
+                  Save Template
+                </Button>
+                <br />
+                <br />
+                {errorMessage.TemplateName == "NoTemplateName" && (
+                  <InlineError
+                    message="Template name is required"
+                    fieldID="TemplateNameErrorField"
+                  />
+                )}
+                <br />
+                <hr />
                 <TextField
                   label="New Option Name"
                   value={optionNameValue}
                   onChange={handleOptionNameChange}
                   placeholder="Example: Option size "
                 />
-                <br/>
-                <Button onClick={add} >Add Option Field</Button>
-
+                <br />
+                <Button onClick={add}>Add Option Field</Button>
+                <br />
+                <br />
+                {errorMessage.OptionName == "error" && (
+                  <InlineError
+                    message={errorMessage.OptionError}
+                    fieldID="optionNameErrorField"
+                  />
+                )}
               </Card>
-
             </Layout.Section>
           </Layout>
         </Layout.Section>
@@ -85,4 +185,3 @@ export default function Template({ shopOrigin }) {
     </Page>
   );
 }
-
